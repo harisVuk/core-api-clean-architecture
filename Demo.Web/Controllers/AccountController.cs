6124ab03160b4ad6e1b935859a6145fccea7dd1f
harisVuk <harisvuk5@gmail.com>
2025-09-25 17:36:40 +0200

    clean_arh_1

---
+++ Demo.Web/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Web.Controllers
+{
+    [Authorize]
+    [Route("/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+    }
+}
